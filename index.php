fb7c5d163d71d3a62c84b6b9b155e4bf13a5235c
angeloflove1980 <gilanggilang0974@gmail.com>
2024-02-18 05:06:10 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,278 @@
+
+<!doctype html>
+<html amp lang="id">
+  <head>
+    <meta charset="utf-8">
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <title>Dewa505 | Login Dewa505 | Link Alternatif Dewa505</title>
+    <link rel="icon" href="https://files.sitestatic.net/ImageFile/20240213190430000000e32177ac8dJOGAAAL__100x100.png" type="image/gif">
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="keywords" content="Dewa505, slot online, slot gacor, pragmaticplay, judi slot online, slot online 2024" />
+    <meta name="description" content="Dewa505, situs dewa505, login dewa505, dewa 505, judi dewa505, link login dewa505, livechat dewa505" />
+    <link rel="canonical" href="http://aerolume.df.ufcg.edu.br/">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+    <style amp-custom>
+         :root{
+                --white: #fff;
+                --max-width: 1080px;
+                --space: min(max(12px, 3.5vmin), 30px);
+                --space-s: min(max(8px, 2vmin), 20px);
+                --space-xs: min(max(4px, 1vmin), 10px);
+                --base: #161616;
+                --primary: #1c1c1c;
+                --secondary: #282828;
+                --secondary-hover: #263041;
+                --tertiary: #919191;
+                --link:#ffffff;
+                --orange: #ca7600;
+                --red: #8c0000;
+                --radius: 10px;
+                --h1: calc(32px + (42 - 32) * ((100vw - 360px) / (1920 - 360)));
+                --gradient:radial-gradient(ellipse farthest-corner at right bottom,#bc0000 0,#bc0000 8%,#bc0000 30%,#c10000 40%,transparent 80%),radial-gradient(ellipse farthest-corner at left top,#fff 0,#c10000 8%,#c20000 25%,#dc0000 62.5%,#ff0000 100%);
+                --pallette-7: #ffffff20;
+            }
+
+        body {
+        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        background:#260000;
+        line-height:1.5;
+        }
+
+        .container {
+            max-width:560px;
+            margin:auto;
+            padding:0 var(--space);
+        }
+        .columns-2{
+            grid-template-columns: repeat(2, 1fr);
+            display:grid;
+        }
+
+        .rounded {
+            border-radius:8px;
+        }
+
+        .hero-inside {
+            color: #fff;
+        }
+        .image {
+            border:3px solid #fff;
+            border-radius: 100px;
+        }
+        .gb-button-8688a7c6 {
+            display: inline-flex;
+            align-items: center;
+            column-gap: 0.5em;
+            color: var(--contrast-2);
+        }
+
+        .gb-block-image img {
+            vertical-align: middle;
+        }
+
+        img {
+            height:auto;
+            max-width: 100%;
+        }
+        figure {
+            display: block;
+            margin-block-start: 1em;
+            margin-block-end: 1em;
+            margin-inline-start: 40px;
+            margin-inline-end: 40px;
+        }
+        .flex {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            column-gap: 10px;
+            z-index: 1;
+            position: relative;
+            margin-top: -25px;
+            margin-right: auto;
+            margin-left: auto;
+        }
+
+        .gb-button-8688a7c6 .gb-icon svg {
+            width: 18px;
+            height: 18px;
+        }
+        .gb-icon {
+            line-height: 0;
+        }
+        .relative {
+            position:relative;
+        }
+        .gb-button {
+            position:absolute;
+            bottom:10px;
+            right:0px;
+        }
+        .title-container{
+            text-align:center;
+        }
+
+        .heading-utama {
+            font-size: 25px;
+            font-weight: 700;
+            margin-bottom: 10px;
+        }
+        .description {
+            font-size: 14px;
+            font-weight: 500;
+        }
+        .nama-situs{
+            color:#fff;
+            column-gap: 0.5em;
+            font-size: 18px;
+            line-height: 1em;
+            font-weight: 700;
+            margin-top: 7px;
+            margin-bottom: 5px
+        }
+        p.headline-nama-situs {
+            font-size: 13px;
+            margin-bottom: 0;
+            color: #fff;
+        }
+        a{
+            text-decoration: none;
+        }
+        .padding-m{
+            padding: var(--space) 0 0 var(--space);
+            background-color: #191919;
+        }
+        .link-collection{
+            background: #191919;
+            border-radius:8px;
+            padding: var(--space);
+        }
+        .link-bio {
+            width:100%;
+            display: flex;
+            align-items: center;
+            column-gap: 8px;
+            position: relative;
+            padding: 8px 0;
+            border-bottom: 1px solid #260000;
+        }
+        .info {
+            font-size: 15px;
+            color:#fff;
+            text-decoration: none;
+            font-weight:500;
+            padding:4px 0;
+        }
+        .link-group-title{
+            font-weight: 500;
+            margin-bottom: 8px;
+            margin-left: 20px;
+            color: #ffffff;
+        }
+        .middle {
+            display:flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+
+        .footer {
+            margin:var(--space) auto;
+            text-align:center;
+        }
+
+        @media only screen and (max-width: 768px) {
+            .heading-utama {
+                line-height:1.3;
+                font-size:19px;
+                margin-bottom:0;
+            }
+            .description{
+                line-height:1.3;
+            }
+            }
+    </style>
+  </head>
+  <body>
+    <section class="container hero-inside" style="margin-top:20px;">
+        <div class="columns-2 padding-m rounded">
+            <div class="middle">
+                <h1 class="heading-utama" title="Dewa505 | Login Dewa505 | Link Alternatif Dewa505">Dewa505 | Login Dewa505 | Link Alternatif Dewa505</h1>
+            </div>
+            <div>
+                <amp-img src="https://www.thecasinoguide.com/wp-content/uploads/2021/12/gates-of-olympus-slot-logo.png" width="550" height="520" layout="responsive" alt="Situs Judi Pragmaticplay X1000 Terbaik Indonesia 2024"></amp-img>
+            </div>
+        </div>
+    </section>
+    <div class="container flex">
+        <div class="relative">
+                <amp-img src="https://dewa505.b-cdn.net/Aset/logo/logo-dewa505.webp" width="80" height="80" class="image" alt="logo dewa505"></amp-img>
+            <span class="gb-button gb-button-8688a7c6 icon_abs abs_ad">
+                <span class="gb-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">
+                        <rect fill="white" height="6" width="8" y="5" x="4"></rect>
+                        <path fill="#0166FF" d="M10.0672 0.870195C9.798 0.594736 9.47649 0.375856 9.12152 0.226418C8.76656 0.0769807 8.38531 0 8.00017 0C7.61503 0 7.23378 0.0769807 6.87882 0.226418C6.52386 0.375856 6.20234 0.594736 5.93317 0.870195L5.31117 1.5082L4.42117 1.4972C4.03592 1.49264 3.65365 1.56515 3.29683 1.71048C2.94002 1.85581 2.61586 2.07102 2.34343 2.34345C2.071 2.61589 1.85579 2.94004 1.71046 3.29686C1.56513 3.65367 1.49261 4.03595 1.49717 4.4212L1.50717 5.3112L0.871172 5.9332C0.595712 6.20237 0.376832 6.52388 0.227395 6.87884C0.0779572 7.23381 0.000976563 7.61506 0.000976562 8.0002C0.000976563 8.38533 0.0779572 8.76659 0.227395 9.12155C0.376832 9.47651 0.595712 9.79803 0.871172 10.0672L1.50817 10.6892L1.49717 11.5792C1.49261 11.9644 1.56513 12.3467 1.71046 12.7035C1.85579 13.0603 2.071 13.3845 2.34343 13.6569C2.61586 13.9294 2.94002 14.1446 3.29683 14.2899C3.65365 14.4352 4.03592 14.5078 4.42117 14.5032L5.31117 14.4932L5.93317 15.1292C6.20234 15.4047 6.52386 15.6235 6.87882 15.773C7.23378 15.9224 7.61503 15.9994 8.00017 15.9994C8.38531 15.9994 8.76656 15.9224 9.12152 15.773C9.47649 15.6235 9.798 15.4047 10.0672 15.1292L10.6892 14.4922L11.5792 14.5032C11.9644 14.5078 12.3467 14.4352 12.7035 14.2899C13.0603 14.1446 13.3845 13.9294 13.6569 13.6569C13.9293 13.3845 14.1446 13.0603 14.2899 12.7035C14.4352 12.3467 14.5077 11.9644 14.5032 11.5792L14.4932 10.6892L15.1292 10.0672C15.4046 9.79803 15.6235 9.47651 15.7729 9.12155C15.9224 8.76659 15.9994 8.38533 15.9994 8.0002C15.9994 7.61506 15.9224 7.23381 15.7729 6.87884C15.6235 6.52388 15.4046 6.20237 15.1292 5.9332L14.4922 5.3112L14.5032 4.4212C14.5077 4.03595 14.4352 3.65367 14.2899 3.29686C14.1446 2.94004 13.9293 2.61589 13.6569 2.34345C13.3845 2.07102 13.0603 1.85581 12.7035 1.71048C12.3467 1.56515 11.9644 1.49264 11.5792 1.4972L10.6892 1.5072L10.0672 0.870195ZM10.3542 6.8542L7.35417 9.8542C7.30773 9.90076 7.25255 9.9377 7.19181 9.96291C7.13106 9.98811 7.06594 10.0011 7.00017 10.0011C6.9344 10.0011 6.86928 9.98811 6.80854 9.96291C6.74779 9.9377 6.69262 9.90076 6.64617 9.8542L5.14617 8.3542C5.09968 8.30771 5.06281 8.25252 5.03765 8.19178C5.01249 8.13104 4.99954 8.06594 4.99954 8.0002C4.99954 7.93445 5.01249 7.86935 5.03765 7.80861C5.06281 7.74787 5.09968 7.69268 5.14617 7.6462C5.19266 7.59971 5.24785 7.56283 5.30859 7.53767C5.36933 7.51251 5.43443 7.49956 5.50017 7.49956C5.56592 7.49956 5.63102 7.51251 5.69176 7.53767C5.75249 7.56283 5.80768 7.59971 5.85417 7.6462L7.00017 8.7932L9.64617 6.1462C9.74006 6.05231 9.8674 5.99956 10.0002 5.99956C10.1329 5.99956 10.2603 6.05231 10.3542 6.1462C10.4481 6.24008 10.5008 6.36742 10.5008 6.5002C10.5008 6.63297 10.4481 6.76031 10.3542 6.8542Z"></path>
+                    </svg>
+                </span>
+            </span>
+        </div>
+        <div class="title-container">
+            <div>
+                <h2 class="nama-situs" title="Dewa505">Dewa505</h2>
+            </div>
+            <p class="headline-nama-situs" title="Bergabunglah dengan Dewa505, situs judi slot online terbaik di Indonesia. Nikmati permainan Pragmaticplay X1000 dan menangkan hadiah besar di tahun 2024!">Bergabunglah dengan Dewa505, situs judi slot online terbaik di Indonesia. Nikmati permainan Pragmaticplay X1000 dan menangkan hadiah besar di tahun 2024!</p>
+        </div>
+    </div>
+    <section class="container">
+        <div>
+            <h3 class="link-group-title">Daftar Link</h3>
+            <div class="link-collection">
+                <div>
+                    <a href="https://betaloop.io/dewa505" class="link-bio">
+                        <amp-img src="https://dewa505.b-cdn.net/Aset/svg/chrome-logo.svg" width="25" height="25" layout="fixed">
+                        </amp-img>
+                        <div class="info">
+                            Link Utama Dewa505 Resmi
+                        </div>
+                    </a>
+                    <a href="https://wa.me/+6287812331987" class="link-bio">
+                        <amp-img src="https://dewa505.b-cdn.net/Aset/svg/whatsapp-circle.svg" width="25" height="25" layout="fixed">
+                        </amp-img>
+                        <div class="info">
+                            +6287812331987
+                        </div>
+                    </a>
+                    <a href="https://sinisaja.org/livechat-dewa505" class="link-bio">
+                        <amp-img src="https://dewa505.b-cdn.net/Aset/svg/livechat.svg" width="25" height="25" layout="fixed">
+                        </amp-img>
+                        <div class="info">
+                            Livechat Dewa505
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container" style="margin-bottom:20px;">
+        <div>
+            <h3 class="link-group-title">Link Alternatif Lain</h3>
+            <div class="link-collection">
+                <div>
+                    <a href="https://dewa505rtplive.com/" class="link-bio">
+                        <amp-img src="https://dewa505.b-cdn.net/Aset/svg/chrome-logo.svg" width="25" height="25">
+                        </amp-img>
+                        <div class="info">
+                            RTP Dewa505
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="container flex footer">
+        <div class="title-container">
+            <p class="headline-nama-situs">Copyright © <a href="http://aerolume.df.ufcg.edu.br/" title="Dewa505">Dewa505</a></p>
+        </div>
+    </footer>
+</body>
+</html>
